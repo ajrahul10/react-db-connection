4a7cacfc1e4aafe47b5aa68e6b4c44da0bb51d50
Rahul Ajmani <rahulajmani910@gmail.com>
2021-06-29 12:21:05 +0530

    using async await

--- src/App.js
+++ src/App.js
@@ -7,22 +7,19 @@ function App() {
 
   const [movies, setMovies] = useState([]);
 
-  function fetchMovieHandler() {
-    fetch('https://swapi.dev/api/films/')
-    .then(response => {
-      return response.json();
+  async function fetchMovieHandler() {
+    const response = await fetch('https://swapi.dev/api/films/')
+    const data = await response.json();
+
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
     })
-    .then(data => {
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies);
-    });
+    setMovies(transformedMovies);
   }
 
   return (
