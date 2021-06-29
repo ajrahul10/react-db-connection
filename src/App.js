693a2363c803c6ff83a4ec0d71c6036ab01ca224
Rahul Ajmani <rahulajmani910@gmail.com>
2021-06-29 12:27:02 +0530

    adding conditional logic for movies

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoaded] = useState(false);
 
   async function fetchMovieHandler() {
+    setIsLoaded(true);
     const response = await fetch('https://swapi.dev/api/films/')
     const data = await response.json();
 
@@ -20,6 +22,7 @@ function App() {
       }
     })
     setMovies(transformedMovies);
+    setIsLoaded(false);
   }
 
   return (
@@ -28,7 +31,9 @@ function App() {
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
